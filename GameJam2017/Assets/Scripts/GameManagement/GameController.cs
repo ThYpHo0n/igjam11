4fb9c634702a6c7a8dd90993f91e77a4e201b290
Scr4py <kevinholst@gmx.net>
2017-07-15 02:04:09 +0200

    ADD: Decision class UPDATE GameController add Decisions

--- GameJam2017/Assets/Scripts/GameManagement/GameController.cs
+++ GameJam2017/Assets/Scripts/GameManagement/GameController.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+
+    private int MonsterFight = 0;
+
     [SerializeField]
     private FirstLevelPictureStory loadManager;
+
+    List<Decisions> decisionList = new List<Decisions>();
+
 	// Use this for initialization
 	void Start ()
     {
@@ -13,7 +19,49 @@ public class GameController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public void IsGoodDecision(bool isGood)
+    {
+        MonsterFight += 1;
+        if (isGood)
+        {
+            decisionList[MonsterFight].RightDecision += 1;
+        }
+        else
+        {
+            decisionList[MonsterFight].WrongDecision += 1;
+        }
+    }
+
+    public void DecisionSummary()
+    {
+        foreach (var item in decisionList)
+        {
+            if (item.WrongDecision > 0)
+            {
+                // TODO Trigger Worng Deciison
+                break;
+            }
+            else
+            {
+                // TODO Trigger Right Decision
+                break;
+            }
+        }
+    }
+
+
+    private void TriggerWrongEnd()
+    {
+        // TODO Here some text.
+    }
+
+    private void TriggerRightEnd()
+    {
+        // TODO Here Some text.
+    }
 }
