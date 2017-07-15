3edfb262b88f642189b62008b2b6862c2b1ba634
Scr4py <kevinholst@gmx.net>
2017-07-16 00:55:33 +0200

    ADDED Intro Scene, Textboxes and More

--- GameJam2017/Assets/Scripts/GameManagement/FirstLevelPictureStory.cs
+++ GameJam2017/Assets/Scripts/GameManagement/FirstLevelPictureStory.cs
@@ -8,11 +8,14 @@ public class FirstLevelPictureStory : MonoBehaviour
 
     [SerializeField]
     private Sprite[] ImageArray;
+    [SerializeField]
+    private Sprite[] TextArray;
 
 
     public int SecondToNextImage = 6;
     public GameObject PanelObject;
     public Image LoadingImage_UI;
+    public Image StoryImage_UI;
 
     private const string KeyToContinue = "Press F to Continue";
 
@@ -25,7 +28,9 @@ public class FirstLevelPictureStory : MonoBehaviour
     void LoadAllImages()
     {
         ImageArray = Resources.LoadAll<Sprite>("LoadingScreenImages/");
+        TextArray = Resources.LoadAll<Sprite>("Begintextboxes/");
         LoadingImage_UI.sprite = ImageArray[0];
+        StoryImage_UI.sprite = TextArray[0];
     }
     public void StartGameStory()
     {
@@ -35,20 +40,37 @@ public class FirstLevelPictureStory : MonoBehaviour
     IEnumerator GameStory()
     {
         LoadingImage_UI.sprite = ImageArray[0];
+        Utility.canWalk = false;
         for (int i = 0; i < ImageArray.Length; i++)
         {
-            Color color = LoadingImage_UI.color;
-            color.a = 1;
             LoadingImage_UI.sprite = ImageArray[i];
-            LoadingImage_UI.color = color;
+            StoryImage_UI.sprite = TextArray[i];
+            
+            if (LoadingImage_UI.color.a < 1f)
+            {
+                do
+                {
+                    LoadingImage_UI.color = Color.Lerp(LoadingImage_UI.color, Color.white, 0.5f * Time.deltaTime);
+                    StoryImage_UI.color = Color.Lerp(StoryImage_UI.color, Color.white, 0.5f * Time.deltaTime);
+                    yield return new WaitForSeconds(0.001f);
+                    if (LoadingImage_UI.color.a > 0.85f)
+                    {
+                        LoadingImage_UI.color = Color.white;
+                        StoryImage_UI.color = Color.white;
+                    }
+                    Debug.Log(LoadingImage_UI.color.a);
+                } while (LoadingImage_UI.color.a < 0.90f);
+            }
             yield return new WaitForSeconds(SecondToNextImage);
             do
             {
                 LoadingImage_UI.color = Color.Lerp(LoadingImage_UI.color, Color.clear, 0.5f * Time.deltaTime);
+                StoryImage_UI.color = Color.Lerp(StoryImage_UI.color, Color.clear, 0.5f * Time.deltaTime);
                 yield return new WaitForSeconds(0.001f);
                 if (LoadingImage_UI.color.a < 0.25f)
                 {
                     LoadingImage_UI.color = Color.clear;
+                    StoryImage_UI.color = Color.clear;
                 }
                 Debug.Log(LoadingImage_UI.color.a);
             } while (LoadingImage_UI.color.a > 0.1f);
@@ -56,5 +78,6 @@ public class FirstLevelPictureStory : MonoBehaviour
         }
 
         PanelObject.SetActive(false);
+        Utility.canWalk = true;
     }
 };
