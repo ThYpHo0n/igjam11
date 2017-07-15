c2fda0955d63e525dee250289ec32a64c716d010
Nik <nik@5on5.org>
2017-07-15 12:54:23 +0200

    ADDED abstract uber class for moving obj

---
+++ GameJam2017/Assets/Scripts/Mobs/MobController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+
+public class MobController : UberControlledObj {
+
+    public override void Update() {
+        
+    }
+
+    private void attack() {
+        //check collision with a player
+        //call decreaseHealth on player obj
+        //move opposite direction? let the player be invincible for x seconds?
+    }
+
+
+}
