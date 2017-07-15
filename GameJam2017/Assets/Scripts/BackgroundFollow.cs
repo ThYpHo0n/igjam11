05f28d5ef1e358eba8c9c16300ecb356d58fc182
Les-Lee <les.dams96@gmail.com>
2017-07-15 11:04:40 +0200

    ADDED:FollowBackground.cs

---
+++ GameJam2017/Assets/Scripts/BackgroundFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour {
+    [SerializeField]
+    private GameObject Background;
+    [SerializeField]
+    private GameObject Player;
+	// Update is called once per frame
+	void Update ()
+    {
+        Background.transform.position = new Vector3(Player.transform.position.x, Background.transform.position.y, Background.transform.position.z); 
+	}
+}
