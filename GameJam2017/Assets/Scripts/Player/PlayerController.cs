9f39f23063a98437e2e5562eb18d1af72a63b186
Daniel_Stein <stein.daniel2606@yahoo.de>
2017-07-15 11:52:09 +0200

    ADDED: -Character -Layer

---
+++ GameJam2017/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D myRB;
+    Animator myAnim;
+
+    //für laufen
+    public float maxSpeed;
+    bool faceRight = true;
+
+    // für Springen
+    public float jumpHigh;
+    bool grounded;
+    float groundCheckRadius = 1f;
+    public LayerMask groundLayer;
+    public Transform groundChecker;
+    Collider2D[] groundCollisions;
+
+	// Use this for initialization
+	void Start () {
+        myRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void FixedUpdate()
+    {
+        groundCollisions = Physics2D.OverlapCircleAll(groundChecker.position, groundCheckRadius, groundLayer);
+
+       
+
+        if (groundCollisions.Length > 0) grounded = true;
+        else grounded = false;
+
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myRB.velocity = new Vector2(myRB.velocity.x, jumpHigh);
+        }
+
+        print(grounded);
+
+        float move = Input.GetAxis("Horizontal");
+        myRB.velocity = new Vector2 (maxSpeed * move, myRB.velocity.y);
+
+        if ((move > 0 && !faceRight) || move < 0 && faceRight) flip();
+    }
+
+    void flip()
+    {
+        faceRight = !faceRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
---
+++ GameJam2017/Assets/Scripts/Player/botTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            collision.gameObject.layer = 8;
+    }
+}
