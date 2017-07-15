60c8983358ad22a072004d6d90bb266de9c5f6a9
Scr4py <kevinholst@gmx.net>
2017-07-15 02:25:40 +0200

    ADD Credit Script, Credit Screen Faded in

---
+++ GameJam2017/Assets/Scripts/Credits/CreditFading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditFading : MonoBehaviour {
+
+    public Image CreditImage;
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine("StartCreditsFading");
+	}
+
+    IEnumerator StartCreditsFading()
+    {
+        do
+        {
+            if (CreditImage.color.a > 0.90f)
+            {
+                CreditImage.color = Color.white;
+            }
+            CreditImage.color = Color.Lerp(CreditImage.color, Color.white, 0.5f * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        } while (CreditImage.color.a < 1f);
+    }
+}
