8d2add973bb195ecf9317b510a45f6deeed3624e
Scr4py <kevinholst@gmx.net>
2017-07-16 13:37:07 +0200

    ADDED: Creditscreen

--- GameJam2017/Assets/Scripts/GameManagement/Menümanager.cs
+++ GameJam2017/Assets/Scripts/GameManagement/Menümanager.cs
@@ -2,11 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menümanager : MonoBehaviour {
 
-	public void StartGame(string scenename)
+    public GameObject CreditCanvas;
+
+    public Image CreditImage;
+
+    private void Start()
+    {
+        CreditCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CreditCanvas.SetActive(false);
+        }
+    }
+
+    public void StartGame(string scenename)
     {
         SceneManager.LoadScene(scenename);
+    }
+
+    public void ShowCredits()
+    {
+        CreditCanvas.SetActive(true);
+        StartCoroutine("StartFade");
+    }
+
+
+    IEnumerator StartFade()
+    {
+        Utility.canWalk = false;
+        if (CreditImage.color.a > 0.85f)
+        {
+            Color color = CreditImage.color;
+            color.a = 0;
+            CreditImage.color = color;
+        }
+        do
+        {
+            if (CreditImage.color.a >= 0.99f)
+            {
+                CreditImage.color = Color.white;
+            }
+            CreditImage.color = Color.Lerp(CreditImage.color, Color.white, 0.5f * Time.deltaTime);
+            yield return new WaitForSeconds(0.001f);
+        } while (CreditImage.color.a < 1f);
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("MainMenu");
     }
 }
